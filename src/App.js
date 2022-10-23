80b7bec0da37a7dd2a0baa32bddf6044da27d5b0
NohaAlghhol <alghoolnoha@gmail.com>
2022-10-23 12:12:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.cssg'
+
+const App = () => {
+ 
+    return (
+       <>
+       <div className='m-5 p-5'>
+        <div className='text-center w-50 primary'>one</div>
+        <div>Two</div>
+       </div>
+      </>
+    );
+}
+export default App;
+
+
+//34 min
